4ea919eedcbf8e63c4c78a4541cb58701342f45b
Daniel <danydeitu18@gmail.com>
2022-10-10 12:06:37 -0300

    Ejercicios py

---
+++ progrmas.py
@@ -0,0 +1,23 @@
+print("Hola Mundo")
+
+nombre = input("Ingrese nombre: ")
+print("Bienvenido a la programacion ", nombre)
+
+numero1 = int(input("Ingrese un numero: "))
+numero2 = int(input("Ingrese un segundo numero: "))
+numero3 = int(input("Ingrese un tercer numero: "))
+
+suma = numero1 + numero2 + numero3
+resta = numero1 - numero2
+
+promedio = float(suma/3)
+
+
+print("La suma de los numeros es: ",suma)
+print("La resta de los numeros es: ",resta)
+print("El promedio de los numeros es:",promedio)
+
+
+
+
+
